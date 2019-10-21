4cbe7b17d0d3825de42fc3420eec9e82e6d04fa4
James <ha.j@grattaninstitute.edu.au>
2019-10-21 13:12:40 +1100

    Uploading data and the script used to wrangle it.

---
+++ wrangle_TableBuilder_output.R
@@ -0,0 +1,41 @@
+### Here we wrangle the raw TableBuilder output
+
+library(tidyverse)
+#devtools::install_github('wfmackey/absmapsdata')
+library(absmapsdata) # Will's very helpful package
+
+# We extracted data from TableBuilder as 'csv string value'
+
+raw_by_sa4 <- read_csv("ABS Stats conf unemp data.csv", 
+                       # There's lots of lines of junk in TableBuilder output
+                       skip = 10, 
+                       col_names = c("dataset",
+                                     "sa4",
+                                     "age", 
+                                     "sex",
+                                     "atsi",
+                                     "education",
+                                     "lfs",
+                                     "count"
+                       ),
+                       col_types = "ccfffffd") %>%
+  filter(!(is.na(lfs))) %>%
+  left_join(sa42016 %>% select(sa4_name_2016, state_name_2016),
+            by = c("sa4" = "sa4_name_2016")) %>%
+  filter(!str_starts(sa4, "Migratory"), 
+         !str_starts(sa4, "No usual")) %>%
+  select(-dataset) %>%
+  rename(state = state_name_2016) %>%
+  mutate(sa4 = factor(sa4), 
+         state = factor(state)) %>%
+  select(state, everything())
+
+glimpse(raw_by_sa4)
+
+write_csv(raw_by_sa4, "unemployment_data_by_sa4.csv")
+
+
+
+
+
+
